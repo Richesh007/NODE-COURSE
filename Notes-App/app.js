c04348814fc06269bf745818601d851859ccbc62
Richesh Gaurav <richeshgaurav@gmail.com>
2020-10-03 19:03:13 +0530

    Learning from Section-3 Chapter-11 (Importing NPM modules) : Up and running with npm modules

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,16 +1,12 @@
 //Learning from Section-3 Chapter-11 (Importing NPM modules) 
-    //To start with NPM modules we need to use NPM commands (NPM stands for Node Package Manager). NPM comes as a core module with Node.js as console.
-    // The very first command is "npm init" which should be ran into root directory of your project. 
-    // It will create a package.json file which is nothing but config file of the project as a default custom Package of the project
-    // It will also contain other NPM package info which is installed using "npm install <package name>" for this project
+    //To get start with a package it's always good to look into the docs section of that package to grab some knowledge about the package
 
-    //Now I'm gonaa start with actual NPM package from "www.npmjs.com" e.g. "validator"
-    //TO use this package in my project I have to use "npm i validator" where i is short for install
-    //this command is common to all the npm packages except for the name of the 
-    //package which we can get from the site "https://www.npmjs.com/package/<packagename> e.g. https://www.npmjs.com/package/validator"
-
-    //"npm i validator" this command will create a file (package-lock.json which is again a config which contains a bit extra info making npm a bit faster
-    // and a bit secure by keeping exact version and the path of the package and SHA code for that package) and a directory (node_modules) which will contain required files/folders of the package installed
-    // we should not modify any of these files/folders as it will be handled by npm
-
-    //And it's not mandatory but always we can specify the version of the package to be installed like "npm i validator@10.8.0" to install specific version
+    const validator = require('validator'); //importing the package same as node core modules as we did earlier
+    const obj = require('./utils.js');
+    console.log(validator.isEmail(obj.email1)); //true
+    console.log(validator.isEmail(obj.email2)); //false
+    console.log(validator.isURL(obj.url1)); //true
+    console.log(validator.isURL(obj.url2)); //true
+    console.log(validator.isURL(obj.url3)); //true
+    console.log(validator.isURL(obj.url4)); //true
+    console.log(validator.isURL(obj.url5)); //false
--- Notes-App/utils.js
+++ Notes-App/utils.js
@@ -1,4 +1,10 @@
-const add = function(a,b) {
-    return a+b;
+
+module.exports = {
+    email1:'richsh.gaurav@gmail.com',
+    email2:'richesh@com',
+    url1:'www.abc.com',
+    url2:'https://www.abc.com',
+    url3:'https://abc.com',
+    url4:'abc.com',
+    url5:'https/abc.com',
 }
-module.exports = add
