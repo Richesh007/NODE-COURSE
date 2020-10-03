31afd9df8ba916bb4d91d2240da8ad664b3fd63e
Richesh Gaurav <richeshgaurav@gmail.com>
2020-10-03 17:53:05 +0530

    Learning from Section-3 Chapter-11 (Importing NPM modules) : npm init

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,7 +1,5 @@
-//Learning from Section-3 Chapter-10 (Importing your own files) 
-    //Challenge
-    
-    const fs = require('fs');
-    fs.writeFileSync('notes.js', 'const getNotes = function() {return \'Your notes...\';} \n module.exports = getNotes');
-    const getNotes = require('./notes.js');
-    console.log(getNotes());
+//Learning from Section-3 Chapter-11 (Importing NPM modules) 
+    //To start with NPM modules we need to use NPM commands (NPM stands for Node Package Manager). NPM comes as a core module with Node.js as console.
+    // The very first command is "npm init" which should be ran into root directory of your project. 
+    // It will create a package.json file which is nothing but config file of the project as a default custom Package of the project
+    // It will also contain other NPM package info which is installed using "npm install <package name>" for this project
