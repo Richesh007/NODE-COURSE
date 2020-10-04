530ce415d9a9fdc93cc918803dc800c3e3efe1c9
Richesh Gaurav <richeshgaurav@gmail.com>
2020-10-05 00:56:49 +0530

    Learning from Section-4 chapter-19 (Adding a Note): Finally added a note

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,19 +1,18 @@
-//Learning from Section-4 chapter-17 (Argument parsing with yargs: part-2)
+//Learning from Section-4 chapter-19 (Adding a Note)
+    // In this chapter finally we are gonna actually adding a note into a json file 
     const chalk = require('chalk');
     const yargs = require('yargs');
-    const getNotes = require('./notes.js')
+    const notes = require('./notes.js')
 
 
-    // Now to make these commands more useful and dynamic we'll use builder property as shown below for add command
-    //this builder property will add some option which can be given with the command or can make it mandatory to run that particular command
     yargs.command({                                 
         command: 'add',                             
         describe: 'This will add a new note.',
         builder: {
-            title: { // this is an option which can be given with add command like 'node app.js add --title="Note title"'
+            title: { 
                 describe: 'Note title',
-                demandOption: true, //this is used to make this option mandatory by default it is false which means optional
-                type: 'string' //this will define the type of option's value
+                demandOption: true, 
+                type: 'string'
             },
             body: {
                 describe: 'Note body',
@@ -21,26 +20,11 @@
                 type: 'string'
             }
         },      
-        handler: function(argv /* here argv is command line arguments */){                        
-            console.log(chalk.bold.blue('Title: ')+ argv.title);       
-            console.log(chalk.bold.blue('body: ')+ argv.body);       
+        handler: function(argv){                        
+            console.log(chalk.bold.green('Adding a note...'));       
+            notes.addNote(argv.title, argv.body);   
         }                                           
-    })   
-
-    // if we run this above add command without required options (i.e. title, body) it will throw below error
-    /* 
-        app.js add
-
-        This will add a new note.
-
-        Options:
-        --help     Show help                                                 [boolean]
-        --version  Show version number                                       [boolean]
-        --title    Note title                                      [string] [required]
-        --body     Note body                                       [string] [required]
-
-        Missing required arguments: title, body
-    */
+    }) 
 
     yargs.command({                                 
         command: 'remove',                             
@@ -64,6 +48,4 @@
         }                                           
     })   
 
-    // args.argv; 
-    // Instead of using the above commented code to parse all the yargs command we can use below function call.
     yargs.parse();
--- Notes-App/notes.js
+++ Notes-App/notes.js
@@ -1,2 +1,33 @@
+const fs = require('fs');
+
 const getNotes = function() {return 'Your notes...';} 
- module.exports = getNotes
+
+const addNote = function(title, body) {
+    const notes = loadNotes();
+
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes);
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch (error) {
+        console.log(error); //this will get executed for the first time as we haven't created notes.json file which we are trying to read
+        return [];
+    }
+}
+
+const saveNotes = function(notes) {
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+}
+
+module.exports = {
+    getNotes,
+    addNote
+}
