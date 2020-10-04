9b8dd4687ce8eadcfecfb37dffc8bb6f044794ac
Richesh Gaurav <richeshgaurav@gmail.com>
2020-10-04 19:32:32 +0530

    Learning from Section-4 chapter-16 (Argument parsing with yargs: part-1): UP and running with yargs

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,44 +1,61 @@
-//Learning from Section-4 chapter-15 (Getting Input from Users)
-    
-    //Like browsers have Window/Document object similarly Node has "Process" object So,
-    // from this object  we will be using "process.argv" here to get input from user through command line
-
-        // console.log(process.argv);
-
-
-    // if we run this js file with an argument like "node app.js arg" then we'll get output as shown below
-
-        // [
-        // 'C:\\Program Files\\nodejs\\node.exe',
-        // 'C:\\Users\\riche\\OneDrive\\Desktop\\Node Course\\NODE-COURSE\\Notes-App\\app.js',
-        // 'arg'
-        // ]
-    
-    //Now here we can see "process.aegv" is an array where first two indexes are same for all
-    //  but the actual argument is the third index which we can use to do something meaningful 
-
-        // console.log("\nHello "+process.argv[2]+"!"); //Run this file with a name as an argument.
-    
-
-    //Now our task is to perform data manipulation into notes.txt by getting command from the user
-    //but before that we'll see a demo using console log
-    
-    const command = process.argv[2];
-
-    if(command === 'add'){
-        console.log('Adding to the note!!');
-    }
-    // node app.js add  
-    // Adding to the note!!
-
-    // Now if we want to make it more dynamic then we have to write parsing logic or we can use any npm package
-    // So, We'll be using "yargs" package
+//Learning from Section-4 chapter-16 (Argument parsing with yargs: part-1)
+    const chalk = require('chalk');
     const yargs = require('yargs');
-    console.log(process.argv);
-        // [
-        //     'C:\\Program Files\\nodejs\\node.exe',
-        //     'C:\\Users\\riche\\OneDrive\\Desktop\\Node Course\\NODE-COURSE\\Notes-App\\app.js',
-        //     'add'
-        // ]
-    console.log(yargs.argv);
-        // { _: [ 'add' ], '$0': 'app.js' }
+    const getNotes = require('./notes.js')
+
+    //The below console log will print 1.0.0 with the command "node app.js --version"
+    //and it will print below 3 lines with command "node app.js -help"
+        //     Options:
+        // --help     Show help                                                 [boolean]
+        // --version  Show version number                                       [boolean]
+    // console.log(yargs.argv) 
+
+    // Customizing yargs version
+    yargs.version('1.1.0') //Now we will get 1.1.0 with the command "node app.js --version"
+
+    // Creating add command
+    yargs.command({                                 
+        command: 'add',                             
+        describe: 'This will add a new note.',      
+        handler: function(){                        
+            console.log('Adding a new note.')       
+        }                                           
+    })      
+    
+    //Now after creating the above command we will get below output by running the command "node app.js -help"
+
+        // Commands:
+        // app.js add  This will add a new note.
+
+        // Options:
+        // --help     Show help                                                 [boolean]
+        // --version  Show version number                                       [boolean]
+
+    // So, Basically it will show the script name, command and discription of the command under the command list.
+    // Now if we give add command in command line it will print "Adding a new note."
+
+    // console.log(yargs.argv);
+
+    yargs.command({                                 
+        command: 'remove',                             
+        describe: 'This will remove a note.',      
+        handler: function(){                        
+            console.log('removing a note.')       
+        }                                           
+    })   
+    yargs.command({                                 
+        command: 'list',                             
+        describe: 'This will list out all notes.',      
+        handler: function(){                        
+            console.log('Printing')       
+        }                                           
+    })   
+    yargs.command({                                 
+        command: 'read',                             
+        describe: 'This will read a note.',      
+        handler: function(){                        
+            console.log('reading a note.')       
+        }                                           
+    })   
+
+    yargs.argv; //this should be present at the end of all yargs related task. this could be inside console log also
